77828197f80b6c9b480468632c098f1beb2edb68
Elvis Knapman <elvis@elvisknapman.com>
2019-04-17 15:33:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Quiz Application
+
+Quiz application written in pure vanilla JavaScript.
+
+<br/>
+
+## Todo
+---
+- Wire up radio buttons
+- validate to make sure user has made a selection before being able to move to next question
+- Keep track of users' answer selections
+- Store users' answers in local storage
+- Ability for user to go back and edit their selection
---
+++ quiz.js
@@ -0,0 +1,40 @@
+const quiz = [
+    {
+        question: "Who holds the record for most career points scored in the NHL?",
+        options: ["Mario Lemieux", "Wayne Gretzky", "Gordie Howe", "Mark Messier"],
+        answer: 1
+    },
+    {
+        question: "What is the most amount of goals scored in a single NHL season?",
+        options: [92, 86, 97, 78],
+        answer: 0
+    },
+    {
+        question: "What is the most total points (goals + assists) scored in a single NHL season?",
+        options: [212, 200, 199, 215],
+        answer: 3
+    }
+
+];
+
+
+// player object holding name, current question number, and answers array
+const player = {
+    name: '',
+    questionNum: 0,
+    answers: [],
+
+    nextQuestion() {
+        // only increase 'questionNum' if current number is below upper boundary
+        if (this.questionNum < quiz.length - 1) {
+        this.questionNum++;
+        }
+    },
+
+    previousQuestion() {
+        // only decrease 'questionNum' if current number is above lower boundary
+        if (this.questionNum > 0) {
+        this.questionNum--;
+        }
+    }
+};
